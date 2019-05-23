9d7d72700457851ced9823c06462ee579d020fe6
davelilly <davelilly@me.com>
2019-05-23 09:56:22 -0600

    simplified file structure

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
 import React, {Component} from 'react'; //Was import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Searchbar from './components/Searchbar';
-import Results from './components/Results';
-import './products.js';
+import Search from './components/Search';
+import products from './products';
 
-class App extends Component{ //Was simply function App() { with no render(){ below
 
-  constructor(props){
-		super(props);
-		this.state={
-			query:''
-		};
-  }
-  
-  handleq (qry) {
-    //console.log("Searching:" + qry)
-    this.setState({'qry': qry.toLowerCase().trim()})
-  }
-  
-  
-  // componentWillReceiveProps(nextProps){
-	// 	var foundProducts = nextProps.products.filter(product => {
-	// 	  return product.name.toLowerCase().match(nextProps.rezults.toLowerCase()) ||
-	// 			   product.description.toLowerCase().match(nextProps.rezults.toLowerCase());
-	// 	});
-	// 	this.setState({
-	// 	  foundProducts: foundProducts
-	// 	});		
-	// 	//console.log("Results:"+ nextProps.rezults);
-	// 	//console.log(nextProps.productz);		
-	// }
+//class App extends Component{ //Was simply function App() { with no render(){ below
 
-	
- render(){
-  return (
-    <div className="App">
-      
-      <h1>homēapp.io</h1>
-      
-      <Searchbar onQuery={this.handleq.bind(this)}/>
-      <Results />
-      
 
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
-}
- } 
   
 
-export default App;
+//export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,106 @@
-import React from 'react';
+import React, {Component} from 'react'; //Was import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
+import products from './products';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Results extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            foundProducts: props.products
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        var foundProducts = nextProps.products.filter(product => {
+            return product.name.toLowerCase().match(nextProps.query.toLowerCase()) ||
+                   product.description.toLowerCase().match(nextProps.query.toLowerCase());
+          });
+          this.setState({
+            foundProducts: foundProducts
+          });
+        console.log("RES: " + nextProps.query);
+    }
+
+    render() {
+        return (
+            <div className="results">
+                <div className="stocked">
+                    <h2>Toothpasted</h2>
+                    <p>$2.99</p>
+                    <p>Lorem ipsum dolor dskdf lkjshd iweh wiu eidushf usdhfoiewn oiowe oiwehfwe oihf</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Searchbar extends Component{
+    
+    handleQuery(event){
+        this.props.onQuery(event.target.value);
+        //console.log("mySEARCH: " + event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <input onChange={this.handleQuery.bind(this)} />
+            </div>
+        )
+    }
+}
+
+
+class Search extends Component{ //Was simply function App() { with no render(){ below
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    handleQuery(query){
+        //console.log("mySEARCHY: " +query.toLowerCase().trim());
+        this.setState({'query': query.toLowerCase().trim()})
+    }
+
+    render() {
+        return (
+          <div className="search">
+            <Searchbar onQuery={this.handleQuery.bind(this)}/>
+            <Results products={this.state.products} query={this.state.query} />
+
+          </div>
+          ); 
+    }
+}
+
+
+class App extends Component{ //Was simply function App() { with no render(){ below
+  
+   render(){
+    return (
+      <div className="App">
+        
+        <h1>homēapp.io</h1>
+        
+        <Search />
+        
+      </div>
+      );
+    }
+  } 
+
+
+
+
+ReactDOM.render(<Search />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
